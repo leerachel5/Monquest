e04000226221db7c2c0eb2c6090b202fcafd00d3
Rachel Lee <leerachel481@gmail.com>
2023-01-22 19:59:24 -0800

    Add Entity Component System

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,11 +3,17 @@
 #include "GameObject.hpp"
 #include "Map.hpp"
 
+#include "ECS.hpp"
+#include "Components.hpp"
+
 GameObject* player;
 GameObject* enemy;
 Map* map;
+
 SDL_Renderer* Game::renderer = nullptr;
 
+Manager manager;
+auto& newPlayer(manager.addEntity());
 
 Game::Game()
 {}
@@ -37,6 +43,9 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height) {
     player = new GameObject("assets/player.png", 0, 0);
     enemy = new GameObject("assets/enemy.png", 50, 50);
     map = new Map();
+    
+    newPlayer.addComponent<PositionComponent>();
+    newPlayer.addComponent<PositionComponent>().setPos(500, 500);
 }
 
 void Game::handleEvents() {
@@ -55,6 +64,9 @@ void Game::handleEvents() {
 void Game::update() {
     player->Update();
     enemy->Update();
+    manager.update();
+    std::cout <<  newPlayer.getComponent<PositionComponent>().x() << ", "
+        << newPlayer.getComponent<PositionComponent>().y() << std::endl;
 }
 
 void Game::render() {
