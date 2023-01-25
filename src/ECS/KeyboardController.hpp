7283c7ac1b6a43477ef11685e07e5745ec2157fb
Rachel Lee <leerachel481@gmail.com>
2023-01-25 02:17:23 -0800

    Fix buggy sprite controls

--- src/ECS/KeyboardController.hpp
+++ src/ECS/KeyboardController.hpp
@@ -15,55 +15,42 @@ public:
     }
     
     void update() override {
-        if (Game::event.type == SDL_KEYDOWN) {
-            switch (Game::event.key.keysym.sym) {
-                case SDLK_w:
-                    transform->velocity.y = -1;
-                    sprite->Play("WalkNorth");
-                    break;
-                case SDLK_a:
-                    transform->velocity.x = -1;
-                    sprite->Play("WalkWest");
-                    break;
-                case SDLK_s:
-                    transform->velocity.y = 1;
-                    sprite->Play("WalkSouth");
-                    break;
-                case SDLK_d:
-                    transform->velocity.x = 1;
-                    sprite->Play("WalkEast");
-                    break;
-                
-                default:
-                    break;
-            }
+        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+        
+        if (!currentKeyStates[SDL_SCANCODE_W]) {
+            transform->velocity.y = 0;
+            sprite->Play("IdleNorth");
         }
-        if (Game::event.type == SDL_KEYUP) {
-            switch (Game::event.key.keysym.sym) {
-                case SDLK_w:
-                    transform->velocity.y = 0;
-                    sprite->Play("IdleNorth");
-                    break;
-                case SDLK_a:
-                    transform->velocity.x = 0;
-                    sprite->Play("IdleWest");
-                    break;
-                case SDLK_s:
-                    transform->velocity.y = 0;
-                    sprite->Play("IdleSouth");
-                    break;
-                case SDLK_d:
-                    transform->velocity.x = 0;
-                    sprite->Play("IdleEast");
-                    break;
-                case SDLK_ESCAPE:
-                    Game::isRunning = false;
-                    break;
-                
-                default:
-                    break;
-            }
+        if (!currentKeyStates[SDL_SCANCODE_A]) {
+            transform->velocity.x = 0;
+            sprite->Play("IdleWest");
         }
+        if (!currentKeyStates[SDL_SCANCODE_S]) {
+            transform->velocity.y = 0;
+            sprite->Play("IdleSouth");
+        }
+        if (!currentKeyStates[SDL_SCANCODE_D]) {
+            transform->velocity.x = 0;
+            sprite->Play("IdleEast");
+        }
+        
+        if (currentKeyStates[SDL_SCANCODE_W]) {
+            transform->velocity.y = -1;
+            sprite->Play("WalkNorth");
+        }
+        if (currentKeyStates[SDL_SCANCODE_A]) {
+            transform->velocity.x = -1;
+            sprite->Play("WalkWest");
+        }
+        if (currentKeyStates[SDL_SCANCODE_S]) {
+            transform->velocity.y = 1;
+            sprite->Play("WalkSouth");
+        }
+        if (currentKeyStates[SDL_SCANCODE_D]) {
+            transform->velocity.x = 1;
+            sprite->Play("WalkEast");
+        }
+
     }
 };
 
